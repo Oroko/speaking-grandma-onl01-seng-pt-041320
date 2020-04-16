584eb15b1434f681eaebe39c5e457feca8e3af57
Abel Gechure <aoroko09@gmail.com>
2020-04-16 05:38:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,6 +16,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
     
-  elsif phrase
+  elsif phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY"
   end
 end
